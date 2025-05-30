0ec653aa8c73ae17b66743658955269c45443da0
“Tea” <tiaogeng@foxmail.com>
2025-05-30 18:28:03 +0800

    更新

--- hm-common/src/main/java/com/hmall/common/config/JsonConfig.java
+++ hm-common/src/main/java/com/hmall/common/config/JsonConfig.java
@@ -1,6 +1,9 @@
 package com.hmall.common.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -12,12 +15,18 @@ import java.math.BigInteger;
 @Configuration
 @ConditionalOnClass(ObjectMapper.class)
 public class JsonConfig {
+
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return jacksonObjectMapperBuilder -> {
+        return builder -> {
+            builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+            builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            builder.featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
             // long -> string
-            jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance);
-            jacksonObjectMapperBuilder.serializerByType(BigInteger.class, ToStringSerializer.instance);
+            builder.serializerByType(Long.class, ToStringSerializer.instance);
+            builder.serializerByType(Long.TYPE, ToStringSerializer.instance);
+            builder.serializerByType(BigInteger.class, ToStringSerializer.instance);
         };
     }
 }
---
+++ hm-common/src/main/java/com/hmall/common/utils/RedisKeyEnum.java
@@ -0,0 +1,38 @@
+package com.hmall.common.utils;
+
+import lombok.Getter;
+
+@Getter
+public enum RedisKeyEnum {
+
+    // === 用户模块 ===
+    USER_INFO("user:{%s}:info"),
+    USER_TOKEN("user:{%s}:token"),
+
+    // === 购物车模块 ===
+    CART_ITEMS("cart:{%s}:items"),
+    CART_LOCK("lock:cart:{%s}:checkout"),
+
+    // === 订单模块 ===         用户id:订单id
+    ORDER_STATUS("order:{%s}:%s:status"),
+    ORDER_LOCK("lock:order:{%s}:%s"),
+
+    // === 商品模块 ===
+    ITEM_STOCK("item:%s:stock"), // 与用户无关，不用 hash tag
+
+    // === 支付模块 ===         用户id:支付id
+    PAY_STATUS("pay:{%s}:%s:status");
+
+    private final String keyPattern;
+
+    RedisKeyEnum(String keyPattern) {
+        this.keyPattern = keyPattern;
+    }
+
+    /**
+     * 根据 pattern 格式化实际 key
+     */
+    public String format(Object... args) {
+        return String.format(this.keyPattern, args);
+    }
+}
