c09100ceebbeb8c1433b9fcf250411cfa6857ff4
“Tea” <tiaogeng@foxmail.com>
2025-06-01 20:54:55 +0800

    订正user客户端

--- hm-api/src/main/java/com/hmall/api/client/UserClient.java
+++ hm-api/src/main/java/com/hmall/api/client/UserClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("user-client")
+@FeignClient("user-service")
 public interface UserClient {
     @PutMapping("/users/money/deduct")
     void deductMoney(@RequestParam("pw") String pw, @RequestParam("amount") Integer amount);
