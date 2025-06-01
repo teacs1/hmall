0f27d37358a5c2030341bdb191b50fbac084b381
“Tea” <tiaogeng@foxmail.com>
2025-06-01 20:56:03 +0800

    订单状态消息变更消费者

---
+++ trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
@@ -0,0 +1,26 @@
+package com.hmall.trade.listener;
+
+import com.hmall.trade.service.IOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PayStatusListener {
+
+    private IOrderService orderService;
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "trade.pay.success.queue", durable = "true"),
+            exchange = @Exchange(name = "pay.direct", type = ExchangeTypes.DIRECT),
+            key = "key.success"
+    ))
+    public void listenSuccess(Long orderId) {
+        orderService.markOrderPaySuccess(orderId);
+    }
+}
